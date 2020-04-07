9d21745f5ab8ccabe7e26271215ef34aa68e4b44
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-04-07 17:39:29 -0300

    parte 2.1 intent mainactivity.java a loginactivity.java

--- app/src/main/java/com/example/loginregister/LoginActivity.java
+++ app/src/main/java/com/example/loginregister/LoginActivity.java
@@ -2,13 +2,35 @@ package com.example.loginregister;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView activityLoginTextViewUsernameRecibido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //linkeo atributo con elemento del xml
+        this.activityLoginTextViewUsernameRecibido = findViewById(R.id.activityLoginTextViewUsernameRecibido);
+
+        //traigo el intent
+        Intent desdeMainActivity = getIntent();
+
+        //le saco el bundle que trae
+        Bundle usernameRecibidoDeIntent = desdeMainActivity.getExtras();
+
+        //le saco al bundle el username y lo guardo en un string
+        String usernameRecibido = usernameRecibidoDeIntent.getString("username");
+
+        //asocio el string al text del textView
+        activityLoginTextViewUsernameRecibido.setText(usernameRecibido);
+
     }
 }
--- app/src/main/java/com/example/loginregister/MainActivity.java
+++ app/src/main/java/com/example/loginregister/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.loginregister;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mainActivityEditTextUsername;
+    private EditText mainMainActivityEditTextPassword;
+    private Button mainActivityButtonLogin;
+    private Button mainActivityButtonRegister;
+    private Map<String, String> usuariosRegistrados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //linkeo los atributos con los elementos del xml
+        this.mainActivityEditTextUsername = findViewById(R.id.mainActivityEditTextUsername);
+        this.mainMainActivityEditTextPassword = findViewById(R.id.mainActivityEditTextPassword);
+        this.mainActivityButtonLogin = findViewById(R.id.mainActivityButtonLogin);
+        this.mainActivityButtonRegister = findViewById(R.id.mainActivityButtonRegister);
+        this.usuariosRegistrados = new HashMap<>(); // para en un futuro agregarle los usuarios que se van registrando
+
+        //seteo el listener del boton login
+        this.mainActivityButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //creo intent: desde donde hasta donde
+                Intent mainALogin = new Intent(MainActivity.this, LoginActivity.class);
+
+                //creo bundle que llevara username
+                Bundle username = new Bundle();
+
+                //agrego username ingresado al bundle creado
+                username.putString("username", mainActivityEditTextUsername.getText().toString());
+
+                //asocio el bundle con el intent
+                mainALogin.putExtras(username);
+
+                //ejecuto el intent
+                startActivity(mainALogin);
+
+            }
+        });
+
+
+
     }
 }
